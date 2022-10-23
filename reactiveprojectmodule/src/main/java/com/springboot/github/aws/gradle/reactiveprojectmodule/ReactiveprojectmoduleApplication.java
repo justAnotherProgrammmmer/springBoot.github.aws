39885d061bb63214c4f1fdcd23c030bbf251f6fd
Divya Pitke <artedivya@gmail.com>
2022-10-24 00:23:44 +0530

    Indial Commit for Spring booot skeleton

---
+++ reactiveprojectmodule/src/main/java/com/springboot/github/aws/gradle/reactiveprojectmodule/ReactiveprojectmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.github.aws.gradle.reactiveprojectmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveprojectmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveprojectmoduleApplication.class, args);
+	}
+
+}
