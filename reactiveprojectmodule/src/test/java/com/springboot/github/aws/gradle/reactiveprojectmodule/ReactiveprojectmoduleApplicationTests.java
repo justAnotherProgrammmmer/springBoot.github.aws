fb4861611d82f8791c994942e1c52699404f278a
Divya Pitke <artedivya@gmail.com>
2022-10-24 00:23:44 +0530

    Indial Commit for Spring booot skeleton

---
+++ reactiveprojectmodule/src/test/java/com/springboot/github/aws/gradle/reactiveprojectmodule/ReactiveprojectmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.github.aws.gradle.reactiveprojectmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveprojectmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
